3b29c6dc421ec5072f336a4f578d8ace121ca7f6
Maksim Razantsau <maksimrazantsau@MacBook-Air-Maksim.local>
2026-05-29 18:16:18 +0200

    lambda params changed

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -75,5 +75,5 @@ resource "aws_lambda_event_source_mapping" "kinesis" {
   event_source_arn  = var.kinesis_stream_arn
   function_name     = aws_lambda_function.inference.arn
   starting_position = "LATEST"
-  batch_size        = 10
+  batch_size        = 1
 }
